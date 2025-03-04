e38b17508244e2d8d5905a4c86e07d32e5c1ac1c
Jopolezi <joao.francisco12123@gmail.com>
2025-03-04 12:56:03 -0300

    Atualizando arquivos

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,2 +1,35 @@
-ScrollReveal({ reset: true })
+ScrollReveal({ reset: false })
 
+const slideLeft = {
+    delay: 500,
+    distance: '20%',
+    origin: 'left'
+};
+
+const slideRight = {
+    delay: 500,
+    distance: '20%',
+    origin: 'right'
+}
+
+const slideTop = {
+    delay: 500,
+    distance: '20%',
+    origin: 'top',
+}
+
+const slideBottom = {
+    delay: 500,
+    distance: '20%',
+    origin: 'bottom'
+}
+
+const slideFade = {
+    delay: 500
+}
+
+ScrollReveal().reveal('#reveal-left', slideLeft);
+ScrollReveal().reveal('#reveal-right', slideRight);
+ScrollReveal().reveal('#reveal-bottom', slideBottom);
+ScrollReveal().reveal('#reveal-top', slideTop);
+ScrollReveal().reveal('#reveal-fade', slideFade);
